ca776de181d40bb5139dc8d7d5d137d8720e7a34
HaiNV18 <nvhai061993@gmail.com>
2026-07-26 20:08:27 +0700

    Update bai10, module2 bai1

--- module2/my-app/src/components/Sidebar/Sidebar.jsx
+++ module2/my-app/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,21 @@
+import { MdDashboard, MdOutlineSettings } from "react-icons/md";
+import { FaRegUser } from "react-icons/fa6";
+
 function Sidebar() {
     return (
         <aside className="sidebar">
-            <ul>
-                <li>Dashboard</li>
-                <li>Users</li>
-                <li>Settings</li>
+            <ul className="menu">
+                <li className="menu-item active">
+                    <MdDashboard /> Dashboard
+                </li>
+
+                <li className="menu-item">
+                    <FaRegUser /> Users
+                </li>
+
+                <li className="menu-item">
+                    <MdOutlineSettings /> Settings
+                </li>
             </ul>
         </aside>
     );
